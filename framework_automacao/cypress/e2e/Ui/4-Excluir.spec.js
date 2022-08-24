b8369e01261e6f9b8dffd5ef03791335af2a2987
LEANDRO MARTINS DE MELO <leandro.melo@viavarejo.com.br>
2022-08-24 14:06:49 -0300

    atualização

---
+++ framework_automacao/cypress/e2e/Ui/4-Excluir.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('US0004 - Funcionalidade: Excluir', () => {
+
+    //beforeEach{() => {
+     //   cy.visit('login')
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('login')
+        cy.get('[data-test="login-email"] > .MuiInputBase-root > .MuiInputBase-input').type('le290308@gmail.com')
+        cy.get('[data-test="login-password"] > .MuiInputBase-root > .MuiInputBase-input').type('290308Le')
+        cy.get('[data-test="login-submit"]').click()
+        cy.get('[data-test="dashboard-welcome"]').should('contain', 'Bem-vindo')
+        cy.get('[data-test="dashboard-deleteProfile"]').click()
+
+
+    });
+});
